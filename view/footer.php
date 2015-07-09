b68cbe977b9756c03ff244b3699702544a5bd5b2
rmit-s3443163-quan-do <s3443163@student.rmit.edu.au>
2015-07-09 12:32:39 +1000

    new

---
+++ view/footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JayDz
+ * Date: 9/07/15
+ * Time: 12:28 PM
+ */
+
+?>
+<footer class="footer">
+    <div class="container">
+        <p class="text-muted"> QuanDo &copy; 2015 RMIT</p>
+    </div>
+</footer>
