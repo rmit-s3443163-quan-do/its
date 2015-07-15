e437536fd69f9f526a677962e71d0bb653e6acd4
rmit-s3443163-quan-do <s3443163@student.rmit.edu.au>
2015-07-15 17:17:20 +1000

    qq

---
+++ view/survey_done.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JayDz
+ * Date: 10/07/15
+ * Time: 12:02 AM
+ */
+
+require_once('./controller/QuestionCtrl.php');
+require_once('./controller/UserCtrl.php');
+
+
+if (!UserCtrl::isDone(4)) {
+    $uid = $_COOKIE['uid'];
+    QuestionCtrl::submitTest(new Answers($uid, 4));
+}
+
+?>
+
+<div class="clear-top"></div>
+<div class="container">
+<div class="jumbotron">
+    <h2>Thanks a lot xD</h2>
+    <p>You can finally check your results now.</p>
+    <p><a class="btn btn-primary btn-lg" href="index.php?p=5" role="button">
+            <span class="glyphicon glyphicon-chevron-right"></span>&nbsp;&nbsp; View Results</a>
+    </p>
+</div>
+</div>
